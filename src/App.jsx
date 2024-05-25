9bba095e2e9e9df8b8903546382d20c0f1325617
Moe Haje <91034104+moehaje@users.noreply.github.com>
2024-05-25 23:41:44 +0200

    howItWorks-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import * as Sentry from '@sentry/react';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
-
-import * as Sentry from '@sentry/react';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
 
 const App = () => {
 
@@ -15,6 +16,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   )
 }
